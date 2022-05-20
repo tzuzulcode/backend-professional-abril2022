14d9f49f6f166f6e8eb0ca83cb5cc53465fd7d16
Tzuzul <mail@tzuzulcode.com>
2022-05-20 12:06:21 -0500

    Pods

--- project-microservices/users/src/index.js
+++ project-microservices/users/src/index.js
@@ -9,6 +9,10 @@ const app = express()
 app.use(express.json())
 app.use(router)
 
+app.get("/health",(req,res)=>{
+    return res.send("OK")
+})
+
 app.listen(port,()=>{
     console.log("Listening on: http://localhost:"+port)
 })
